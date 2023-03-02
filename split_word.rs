989ca27166a4bdf36aad73c33addfeec92c560a3
kreangkrai <kreangkrai.ratt@gmail.com>
2023-03-02 11:25:05 +0700

    Create split_word.rs

---
+++ split_word.rs
@@ -0,0 +1,22 @@
+use regex::Regex;
+fn main() -> Result<(),regex::Error>{
+     let text = "Crypto.BTC/USD,Crypto.ETH/USD,Crypto.SOL/USD";
+     let re = Regex::new(r"\.(?P<name>\w+)/")?;
+     let res = re.captures_iter(text);
+     for mat in res{
+          println!("Name : {:?}",&mat["name"]);        
+     }
+     Ok(())
+}
+============================================================
+ fn main() {
+    let text = "Crypto.BTC/USD,Crypto.ETH/USD,Crypto.SOL/USD";
+    let symbols:Vec<&str>  = text
+                         .chars()
+                         .enumerate()
+                         .filter(|(_,c)| *c == '.')
+                         .map(|(i,_)|{
+              let slash = text[(i+1)..].find('/').unwrap();                                                                         &text[(i+1)..(i + slash +1)] as &str
+    }).collect();
+    println!("{:?}",symbols);
+}
