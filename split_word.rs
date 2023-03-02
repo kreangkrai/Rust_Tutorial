4388b12fd0365b8682a6095fe9f152ac028f69bb
kreangkrai <kreangkrai.ratt@gmail.com>
2023-03-02 11:26:53 +0700

    Update split_word.rs

--- split_word.rs
+++ split_word.rs
@@ -11,12 +11,12 @@ fn main() -> Result<(),regex::Error>{
 ============================================================
  fn main() {
     let text = "Crypto.BTC/USD,Crypto.ETH/USD,Crypto.SOL/USD";
-    let symbols:Vec<&str>  = text
+    let word:Vec<&str>  = text
                          .chars()
                          .enumerate()
                          .filter(|(_,c)| *c == '.')
                          .map(|(i,_)|{
               let slash = text[(i+1)..].find('/').unwrap();                                                                         &text[(i+1)..(i + slash +1)] as &str
     }).collect();
-    println!("{:?}",symbols);
+    println!("{:?}",word);
 }
