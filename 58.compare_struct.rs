44fd3e31232f7c86607f714b9a0f2abad0ed69c7
kreangkrai <kreangkrai.ratt@gmail.com>
2023-03-24 09:51:27 +0700

    Create 58.compare_struct.rs

---
+++ 58.compare_struct.rs
@@ -0,0 +1,23 @@
+//Compare Struc
+#[derive(Debug)]
+struct Point<T> {
+    x:T,
+    y:T,
+}
+impl <T> Point<T> {
+    fn new(x:T,y:T)->Self{
+        Point { x: x, y: y }
+    }
+}
+impl <T:PartialEq> PartialEq for Point<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+fn main() {
+    let p1 = Point::new(1,2);
+    let p2 = Point::new(1,2);
+
+    let p = p1 == p2;
+    println!("{}",p);
+}
